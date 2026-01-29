c57e58e638e3439fc64a95f4b7dca47a93b6955a
Thang Dinh <dinhthang6c@gmail.com>
2026-01-29 22:43:12 +0700

    Setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+import setuptools
+
+def run(*cmd):
+    wd = os.path.dirname(os.path.abspath(__file__))
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=wd)
+    return p.stdout.read().decode().rstrip()
+
+
+tag = run('git', 'for-each-ref', '--format=%(refname:short)', '--sort=-authordate', '--count=1', 'refs/tags')
+rev = run('git', 'rev-parse', '--short=8', 'HEAD')
+
+setuptools.setup(
+    name='pyutils',
+    version=f'{tag}+{rev}'
+    author='Thang Dinh'
+    author_email='',
+    packages=["execution_domain"]
+)
